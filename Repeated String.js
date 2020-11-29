618d86ee428799da59ab130393138cb75522dd1a
SiEr0212 <simonertel@gmx.de>
2020-11-29 23:48:57 +0100

    Repeated String

--- Repeated String.js
+++ Repeated String.js
@@ -9,7 +9,7 @@ Given an integer, , find and print the number of letter a's in the first  letter
 
 For example, if the string  and , the substring we consider is , the first  characters of her infinite string. There are  occurrences of a in the substring.
 
-Function Description
+Function Description:
 
 Complete the repeatedString function in the editor below. It should return an integer representing the number of occurrences of a in the prefix of length  in the infinitely repeating string.
 
@@ -17,9 +17,26 @@ repeatedString has the following parameter(s):
 
 s: a string to repeat
 n: the number of characters to consider
-Input Format
+
+Input Format:
 
 The first line contains a single string, s.
 The second line contains an integer, n.
 
+Output Format:
+
+Print a single integer denoting the number of letter a's in the first  letters of the infinite string created by repeating  infinitely many times.
+
+Sample Input 0
+aba
+10
+Sample Output 0
+7
+Explanation 0
+The first  letters of the infinite string are abaabaabaa. Because there are  a's, we print  on a new line.
+Sample Input 1
+a
+1000000000000
+Sample Output 1
+1000000000000
 */
